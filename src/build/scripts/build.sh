69fae42e113fc4e51e3fa4e24102672b194defe1
Emin Karayel <me@eminkarayel.de>
2015-07-04 20:49:03 +0200

    Update build.sh

--- src/build/scripts/build.sh
+++ src/build/scripts/build.sh
@@ -1,5 +1,5 @@
 export GIT_COMMIT_HASH=$(git log --format=%H -n1)
-export GIT_DESCRIBE=$(git describe)
+export GIT_DESCRIBE=$(git describe --always)
 runhaskell src/build/hs/CabalTemplate.hs
 cabal update
 cabal install
