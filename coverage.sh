76bfbed4b11e11031dc2e6e8016932567512786a
Emin Karayel <me@eminkarayel.de>
2015-08-12 01:41:14 +0200

    Bugfix in coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -5,5 +5,4 @@ cd codecov
 cabal sandbox init
 cabal install codecov-haskell
 cd ..
-mv dist/hpc/vanilla/* dist/hpc/
 codecov/.cabal-sandbox/bin/codecov-haskell main --token=$CODECOV_TOKEN 2>&1 | grep -v $CODECOV_TOKEN
