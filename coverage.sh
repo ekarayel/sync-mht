6f4472b6d0051f1952916a80d6addcc20520e211
Emin Karayel <me@eminkarayel.de>
2015-08-31 05:11:43 +0200

    Cloud based benchmarks 2

--- coverage.sh
+++ coverage.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-set -e
-mkdir codecov
-cd codecov
-cabal sandbox init
-cabal install codecov-haskell
-cd ..
+mkdir codecov && cd codecov && cabal sandbox init && cabal install codecov-haskell && cd ..
 codecov/.cabal-sandbox/bin/codecov-haskell main --token=$CODECOV_TOKEN 2>&1 | grep -v $CODECOV_TOKEN
