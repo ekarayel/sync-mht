cca9f0ae1c0c62081cfe34a01f1c975e848cae10
Emin Karayel <me@eminkarayel.de>
2015-09-06 05:11:55 +0200

    Improved Java Code

--- benchmarks/src/main/java/com/github/ekarayel/syncmht/benchmarks/Main.java
+++ benchmarks/src/main/java/com/github/ekarayel/syncmht/benchmarks/Main.java
@@ -5,87 +5,79 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.compute.Compute;
+import com.google.api.services.compute.ComputeScopes;
 import com.google.api.services.compute.model.*;
+import com.google.api.services.storage.StorageScopes;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
-    private static final String SOURCE_IMAGE_PREFIX =
-            "https://www.googleapis.com/compute/v1/projects/";
-    private static final String SOURCE_IMAGE_PATH =
-            "ubuntu-os-cloud/global/images/ubuntu-1504-vivid-v20150616a";
-
-    private static final String NETWORK_INTERFACE_CONFIG = "ONE_TO_ONE_NAT";
-    private static final String NETWORK_ACCESS_CONFIG = "External NAT";
+    private static final String PROJECT_PREFIX = "https://www.googleapis.com/compute/v1/projects/";
 
     public static void main(String[] args) throws GeneralSecurityException, IOException {
         HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
         GoogleCredential cred = Authorize.getCredential();
 
         Compute compute =
-                new Compute.Builder(httpTransport, JacksonFactory.getDefaultInstance(), cred)
-                .setApplicationName(Constants.APP_NAME)
-                .build();
+            new Compute.Builder(httpTransport, JacksonFactory.getDefaultInstance(), cred)
+            .setApplicationName(Constants.APP_NAME)
+            .build();
 
-        Instance instance = new Instance();
-        SecureRandom random = new SecureRandom();
         String instanceName = "sync-mht-benchmarks-instance-"+
-                new BigInteger(130, random).toString(32);
+            new BigInteger(130, new SecureRandom()).toString(32);
 
-        ServiceAccount account = new ServiceAccount();
-        account.setEmail("default");
-        List<String> scopes = new ArrayList<>();
-        scopes.add("https://www.googleapis.com/auth/devstorage.full_control");
-        scopes.add("https://www.googleapis.com/auth/compute");
-        account.setScopes(scopes);
-        instance.setServiceAccounts(Collections.singletonList(account));
-        instance.setName(instanceName);
-        instance.setMachineType("https://www.googleapis.com/compute/v1/projects/"
-                + Constants.PROJECT_ID + "/zones/" + Constants.ZONE_NAME
-                + "/machineTypes/n1-standard-1");
-        NetworkInterface ifc = new NetworkInterface();
-        ifc.setNetwork("https://www.googleapis.com/compute/v1/projects/"
-                + Constants.PROJECT_ID + "/global/networks/default");
-        List<AccessConfig> configs = new ArrayList<>();
-        AccessConfig config = new AccessConfig();
-        config.setType(NETWORK_INTERFACE_CONFIG);
-        config.setName(NETWORK_ACCESS_CONFIG);
-        configs.add(config);
-        ifc.setAccessConfigs(configs);
-        instance.setNetworkInterfaces(Collections.singletonList(ifc));
-
-        //AttachedDisk disk =
-        instance.setDisks(Collections.singletonList(
-            new AttachedDisk()
-            .setBoot(true)
-            .setAutoDelete(true)
-            .setType("PERSISTENT")
-            .setInitializeParams(
-                new AttachedDiskInitializeParams()
-                .setDiskName(instanceName)
-                .setSourceImage(SOURCE_IMAGE_PREFIX + SOURCE_IMAGE_PATH)
-                .setDiskType(
-                    SOURCE_IMAGE_PREFIX  + Constants.PROJECT_ID + "/zones/" + Constants.ZONE_NAME
-                    + "/diskTypes/pd-standard"
+        Instance instance =
+            new Instance()
+            .setServiceAccounts(Collections.singletonList(
+                new ServiceAccount()
+                .setEmail("default")
+                .setScopes(
+                    Arrays.asList(StorageScopes.DEVSTORAGE_FULL_CONTROL, ComputeScopes.COMPUTE)
                 )
+            )).setName(instanceName)
+            .setMachineType(
+                PROJECT_PREFIX + Constants.PROJECT_ID + "/zones/" + Constants.ZONE_NAME
+                + "/machineTypes/n1-standard-1"
+            ).setNetworkInterfaces(Collections.singletonList(
+                new NetworkInterface()
+                .setNetwork(PROJECT_PREFIX + Constants.PROJECT_ID + "/global/networks/default")
+                .setAccessConfigs(Collections.singletonList(
+                    new AccessConfig()
+                    .setType("ONE_TO_ONE_NAT")
+                    .setName("External NAT")
+                ))
+            )).setDisks(Collections.singletonList(
+                new AttachedDisk()
+                .setBoot(true)
+                .setAutoDelete(true)
+                .setType("PERSISTENT")
+                .setInitializeParams(
+                    new AttachedDiskInitializeParams()
+                    .setDiskName(instanceName)
+                    .setSourceImage(
+                        PROJECT_PREFIX +
+                        "ubuntu-os-cloud/global/images/ubuntu-1504-vivid-v20150616a"
+                    ).setDiskType(
+                        PROJECT_PREFIX + Constants.PROJECT_ID + "/zones/" + Constants.ZONE_NAME +
+                        "/diskTypes/pd-standard"
+                    )
+                )
+            )).setMetadata(
+                new Metadata()
+                .setItems(Collections.singletonList(
+                    new Metadata.Items()
+                    .setKey("startup-script")
+                    .setValue(StartupScript.get(instanceName))
+                ))
+            );
 
-            )
-        ));
-
-        Metadata meta = new Metadata();
-        Metadata.Items item = new Metadata.Items();
-        item.setKey("startup-script");
-        item.setValue(StartupScript.get(instanceName));
-        meta.setItems(Collections.singletonList(item));
-        instance.setMetadata(meta);
-        Operation insert =
-                compute.instances().insert(Constants.PROJECT_ID, Constants.ZONE_NAME, instance)
-                .execute();
+        compute.instances().insert(Constants.PROJECT_ID, Constants.ZONE_NAME, instance).execute();
     }
 }
